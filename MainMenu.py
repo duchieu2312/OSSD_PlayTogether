66bb41581f2534fc6c4504222373893821544281
Linh <linhdao2468@example.com>
2024-05-08 23:44:25 +0700

    third version

--- MainMenu.py
+++ MainMenu.py
@@ -1,14 +1,16 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QMessageBox
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QIcon, QPixmap, QFont
 from SpotifyDownloader import SpotifyWindow
 from YoutubeDownloader import YoutubeWindow
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PlayTogether")
         self.resize(800, 600)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("images/Logo.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon = QIcon()
+        icon.addPixmap(QPixmap("images/Logo.jpg"), QIcon.Normal, QIcon.Off)
         self.setWindowIcon(icon)
 
         self.init_ui()
@@ -16,12 +18,12 @@ class MainWindow(QtWidgets.QMainWindow):
     def init_ui(self):
 
         # Khung
-        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget = QWidget(self)
 
         # Nút chuyển sang giao diện Youtube Downloader
-        self.YoutubeButton = QtWidgets.QPushButton(self.centralwidget)
-        self.YoutubeButton.setGeometry(QtCore.QRect(30, 60, 261, 430))
-        font = QtGui.QFont()
+        self.YoutubeButton = QPushButton(self.centralwidget)
+        self.YoutubeButton.setGeometry(QRect(30, 60, 261, 430))
+        font = QFont()
         font.setFamily("Arial Black")
         font.setPointSize(14)
         font.setBold(True)
@@ -31,9 +33,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.YoutubeButton.clicked.connect(self.open_youtubedownloader)
 
         # Nút chuyển sang giao diện Spotify Downloader
-        self.SpotifyButton = QtWidgets.QPushButton(self.centralwidget)
-        self.SpotifyButton.setGeometry(QtCore.QRect(510, 60, 261, 430))
-        font = QtGui.QFont()
+        self.SpotifyButton = QPushButton(self.centralwidget)
+        self.SpotifyButton.setGeometry(QRect(510, 60, 261, 430))
+        font = QFont()
         font.setFamily("Arial Black")
         font.setPointSize(14)
         font.setBold(True)
@@ -43,9 +45,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.SpotifyButton.clicked.connect(self.open_spotifydownloader)
 
         # Tiêu đề
-        self.Title = QtWidgets.QLabel(self.centralwidget)
-        self.Title.setGeometry(QtCore.QRect(250, 0, 311, 51))
-        font = QtGui.QFont()
+        self.Title = QLabel(self.centralwidget)
+        self.Title.setGeometry(QRect(250, 0, 311, 51))
+        font = QFont()
         font.setFamily("Arial Black")
         font.setPointSize(30)
         font.setBold(False)
@@ -55,9 +57,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.Title.setWordWrap(False)
 
         # Giới thiệu
-        self.MadeByLabel = QtWidgets.QLabel(self.centralwidget)
-        self.MadeByLabel.setGeometry(QtCore.QRect(10, 510, 251, 81))
-        font = QtGui.QFont()
+        self.MadeByLabel = QLabel(self.centralwidget)
+        self.MadeByLabel.setGeometry(QRect(10, 510, 251, 81))
+        font = QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
@@ -69,22 +71,22 @@ class MainWindow(QtWidgets.QMainWindow):
 "Nguyễn Trương Tấn Lộc - 3120410293")
         
         # Ảnh nền
-        self.Photo = QtWidgets.QLabel(self.centralwidget)
-        self.Photo.setGeometry(QtCore.QRect(-4, -8, 811, 611))
+        self.Photo = QLabel(self.centralwidget)
+        self.Photo.setGeometry(QRect(-4, -8, 811, 611))
         self.Photo.setText("")
-        self.Photo.setPixmap(QtGui.QPixmap("images/bg.png"))
+        self.Photo.setPixmap(QPixmap("images/bg.png"))
         self.Photo.setScaledContents(True)
 
         # Thông tin
-        self.Desc = QtWidgets.QLabel(self.centralwidget)
-        self.Desc.setGeometry(QtCore.QRect(300, 110, 201, 81))
-        font = QtGui.QFont()
+        self.Desc = QLabel(self.centralwidget)
+        self.Desc.setGeometry(QRect(300, 110, 201, 81))
+        font = QFont()
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
         self.Desc.setFont(font)
         self.Desc.setText("<html><head/><body><p>Choose your downloader</p></body></html>")
-        self.Desc.setAlignment(QtCore.Qt.AlignCenter)
+        self.Desc.setAlignment(Qt.AlignCenter)
 
         self.Photo.raise_()
         self.YoutubeButton.raise_()
@@ -96,19 +98,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.centralwidget)
 
     def open_youtubedownloader(self):
-        self.hide()
         self.youtube_window = YoutubeWindow()
         self.youtube_window.show()
 
     def open_spotifydownloader(self):
-        self.hide()
         self.spotify_window = SpotifyWindow()
         self.spotify_window.load_songs("XXXTentacion")
         self.spotify_window.show()
 
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Exit Confirmation', 'Are you sure you want to exit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+    
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.ApplicationAttribute.AA_DisableWindowContextHelpButton) 
     MainWindow = MainWindow()
     MainWindow.show()
     sys.exit(app.exec_())
